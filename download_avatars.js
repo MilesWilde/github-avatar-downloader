76d7b762f13ef792b6200efb939dd76b37bc4734
MilesWilde <craigmilde_@hotmail.com>
2017-09-11 20:17:43 +0000

    step 9

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,8 @@ let repoOwner = args[0];
 let repoName = args[1];
 var GITHUB_USER = "mileswilde";
 var GITHUB_TOKEN = "9b9898d1c9298aa67287e3973074a3ac1a275618";
-
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var filePath = "./contributorAvatars/downloaded"
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -21,24 +22,37 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 
   let toBeIterated = request.get(options, function(error, response, body) {
-    
-    // console.log("body: ", body);
     let bodyObject = JSON.parse(body);
-    // let bodyArray = body.split('},')
-    // for (let i = 0; i < bodyArray.length-1; i++){
-    //   bodyArray[i] += "}";
-    // }
-    // console.log(bodyArray[5]);
-    //bodyArray[bodyArray.length - 1] = bodyArray[bodyArray.length - 1].substring(0,bodyArray.length - 2)
-    //console.log(bodyArray[bodyArray.length - 1].login);
     for (let currentBodyObject of bodyObject){
-      console.log(currentBodyObject.avatar_url)
+      request.get(currentBodyObject.avatar_url)
+        .on('error', function (err) {                               
+         throw err; 
+       })
+       .on('response', function (response) {                       
+         console.log('Response Status Code: ', response.statusCode);
+         console.log(response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream(filePath + currentBodyObject.login)); 
     }
   })              
 }
 
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
+       .on('error', function (err) {                               
+         throw err; 
+       })
+       .on('response', function (response) {                       
+         console.log('Response Status Code: ', response.statusCode);
+         console.log(response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream(filePath)); 
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+// getRepoContributors(repoOwner, repoName, downloadImageByURL);
+
+getRepoContributors(repoOwner, repoName, function(err, result) {
 
   console.log("Errors:", err);
 
