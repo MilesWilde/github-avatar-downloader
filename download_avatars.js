22290cfc9bd1d83a9369ac2e2516b90434944dc8
MilesWilde <craigmilde_@hotmail.com>
2017-09-11 20:46:51 +0000

    final commit

--- download_avatars.js
+++ download_avatars.js
@@ -21,24 +21,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   }
 
-  let toBeIterated = request.get(options, function(error, response, body) {
+  //gets data from requestURL (in this case, the picture locations)
+  //downloads each picture into specified filepath with named pictures based on login info
+  request.get(options, function(error, response, body) {
     let bodyObject = JSON.parse(body);
     for (let currentBodyObject of bodyObject){
-      request.get(currentBodyObject.avatar_url)
-        .on('error', function (err) {                               
-         throw err; 
-       })
-       .on('response', function (response) {                       
-         console.log('Response Status Code: ', response.statusCode);
-         console.log(response.headers['content-type']);
-       })
-       .pipe(fs.createWriteStream(filePath + currentBodyObject.login)); 
+      downloadImageByURL(currentBodyObject.avatar_url, filePath, currentBodyObject.login)
     }
   })              
 }
 
-function downloadImageByURL(url, filePath) {
-  // ...
+function downloadImageByURL(url, filePath, login) {
   request.get(url)
        .on('error', function (err) {                               
          throw err; 
@@ -47,10 +40,10 @@ function downloadImageByURL(url, filePath) {
          console.log('Response Status Code: ', response.statusCode);
          console.log(response.headers['content-type']);
        })
-       .pipe(fs.createWriteStream(filePath)); 
+       .pipe(fs.createWriteStream(filePath + login)); 
 }
 
-// getRepoContributors(repoOwner, repoName, downloadImageByURL);
+//runs code if there is a repoName&owner given in the node terminal
 if (repoOwner && repoName && repoOwner !== "" && repoName !== ""){
 
   getRepoContributors(repoOwner, repoName, function(err, result) {
@@ -63,7 +56,8 @@ if (repoOwner && repoName && repoOwner !== "" && repoName !== ""){
 
   });
 }
-else{
+//else returns how to enter data that will run code
+else {
   console.log("please enter a repoName and a repoOwner:")
   console.log("usage:")
   console.log("\tnode download_avatars repoName repoOwner")
